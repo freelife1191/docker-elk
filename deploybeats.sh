65892da0b6be518b050e648d09491b1fff9a6ac3
mskwon <mskwon@tidesquare.com>
2023-10-08 17:11:29 +0900

    modify docker compose

--- deploybeats.sh
+++ deploybeats.sh
@@ -4,3 +4,4 @@ docker compose -f auditbeat-docker-compose.yml up -d --build
 docker compose -f filebeat-docker-compose.yml up -d --build
 docker compose -f metricbeat-docker-compose.yml up -d --build
 docker compose -f packetbeat-docker-compose.yml up -d --build
+docker compose -f heartbeat-docker-compose.yml up -d --build
--- removebeats.sh
+++ removebeats.sh
@@ -4,3 +4,4 @@ docker compose -f auditbeat-docker-compose.yml down
 docker compose -f filebeat-docker-compose.yml down
 docker compose -f metricbeat-docker-compose.yml down
 docker compose -f packetbeat-docker-compose.yml down
+docker compose -f heartbeat-docker-compose.yml down
